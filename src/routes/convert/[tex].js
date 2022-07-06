0a6a04e8b24d8b4054464bfc170dd375329fc86b
Uncodeable864 <49599682+Uncodeable864@users.noreply.github.com>
2022-07-06 10:12:24 -0500

    Done!

--- src/routes/convert/[tex].js
+++ src/routes/convert/[tex].js
@@ -1,27 +1,23 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 
-import { svg2png, initialize } from 'svg2png-wasm';
+import sharp from 'sharp'
 
 /** @type {import('./__types/[tex]').RequestHandler} */
 export async function get(request) {
 
     const tex = request.params.tex;
-    const json = await request.json;
-    console.log(request.url.origin); // Use `request.url.origin` to get URL where the request is coming from, and the tack on /svg2png_wasm_bg.wasm to get the web assembly file
 
     const responce = await fetch(`https://math.vercel.app/?from=${tex}`);
-    const svg = responce.body;
+    const svg = await responce.text();
+    const png = await svg2png(svg);
 
-    await initialize(
-        fetch(request.url.origin + "/svg2png_wasm_bg.wasm'")
-    );
+    return { status: 200, body: png };
+}
 
-    const png = svg2png(svg)
-
-    // console.log(`${request.url.origin}/svg2png_wasm_bg.wasm`)
-
-    // return { status: 400 }
-
-    return { status: 200, body: png, headers: { 'Content-Type': 'image/png' } };
+async function svg2png(_svg) {
+    const svg = await sharp(Buffer.from(_svg), { density: 1000 });
+    const _png = await svg.resize({ width: 200 }).png({ quality: 100 });
+    const png = await _png.toBuffer();
+    return png;
 }
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,18 @@
 <h1>LaTeX to Image API</h1>
 <p>Convert LaTeX to image</p>
 
+<h2>
+	Credits:
+	<ul>
+		<li>
+			<a href="https://twitter.com/uechz">@uechz for creating</a><a
+				href="https://math.vercel.app/home">the Math API</a
+			>
+		</li>
+		<li><a href="https://sharp.pixelplumbing.com/">The people behind Sharpjs</a></li>
+	</ul>
+</h2>
+
 <style>
 	@import url('https://rsms.me/inter/inter.css');
 	* {
